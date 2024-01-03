d685db0a9a2222e12b3751b082421c4cb56ad115
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-01-03 22:53:34 +0800

    2

--- 0103/L7.py
+++ 0103/L7.py
@@ -54,3 +54,22 @@ print("借出日期:", b1.get_borrow_date())
 print("還書期限:", b1.get_borrow_deadline())
 print("館藏位置:", b1.get_location())
 
+
+"""副函式EX
+class A(object):
+    #副函式定義，名稱為method1，參數有a,b,c
+    def method1(self, a, b, c):
+        # 副函式程式內容，計算3個數字相乘的積
+        return a*b*c
+           
+ 
+#呼叫副函式，先建立類別物件
+obj1 = A()
+#再利用建立好的類別物件obj1呼叫副函式method1
+a=1
+b=2
+c=3
+result = obj1.method1(a, b, c) #呼叫副函式method1，將結果存入變數result中
+print(result) #列印結果
+
+"""
