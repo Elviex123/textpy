c0a2c4221f086a9743c90c0314abab1afd6d7b5e
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-01-04 00:01:05 +0800

    3

--- 0103/L7.py
+++ 0103/L7.py
@@ -1,3 +1,8 @@
+#考試版
+#注意縮排和是否有冒號,class&def最後要:
+#print加上屬性後要加()
+
+
 class Library:
     def __init__(self,book_name,book_id,book_author,book_publisher,bookpublisher_date,borrow_if,borrow_date,borrow_ddl,book_location,student_name,student_id,student_email):
         self.book_name = book_name
@@ -25,7 +30,7 @@ class Library:
     def get_book_publisher(self):
         return self.book_publisher
     
-    def get_publisher_date(self):
+    def get_bookpublisher_date(self):
         return self.bookpublisher_date
     
     def is_borrowed(self):
@@ -38,8 +43,16 @@ class Library:
         return self.borrow_ddl
     
     def get_location(self):
-        return self.book_location
+        return self.book_location 
     
+    def get_student_name(self):
+        return self.student_name
+    
+    def get_student_id(self):
+        return self.student_id
+    
+    def get_student_email(self):
+        return self.student_email
 # 建立物件
 b1= Library('PYTHON','111','Author','Publisher','2022-01-01','Yes','2023-01-01','2023-02-01','A103','Sname','Sid','Semail')
 
@@ -48,28 +61,11 @@ print("書名:", b1.get_book_name())
 print("書ID:", b1.get_book_id())
 print("作者:", b1.get_book_author())
 print("出版社:", b1.get_book_publisher())
-print("出版日期:", b1.get_publisher_date())
+print("出版日期:", b1.get_bookpublisher_date())
 print("是否借出:", b1.is_borrowed())
 print("借出日期:", b1.get_borrow_date())
 print("還書期限:", b1.get_borrow_deadline())
 print("館藏位置:", b1.get_location())
-
-
-"""副函式EX
-class A(object):
-    #副函式定義，名稱為method1，參數有a,b,c
-    def method1(self, a, b, c):
-        # 副函式程式內容，計算3個數字相乘的積
-        return a*b*c
-           
- 
-#呼叫副函式，先建立類別物件
-obj1 = A()
-#再利用建立好的類別物件obj1呼叫副函式method1
-a=1
-b=2
-c=3
-result = obj1.method1(a, b, c) #呼叫副函式method1，將結果存入變數result中
-print(result) #列印結果
-
-"""
+print("學生姓名:",b1.get_student_name())
+print("學生ID:",b1.get_student_id())
+print("學生EMAIL:",b1.get_student_email())
--- 0103/L8.py
+++ 0103/L8.py
@@ -75,3 +75,22 @@ print("修改後借出日期:", b1.get_borrow_date())
 print("修改前還書期限:", b1.get_borrow_deadline())
 b1.set_borrow_deadline('2023-03-15')  # 修改還書期限
 print("修改後還書期限:", b1.get_borrow_deadline())
+
+"""副函式EX
+class A(object):
+    #副函式定義，名稱為method1，參數有a,b,c
+    def method1(self, a, b, c):
+        # 副函式程式內容，計算3個數字相乘的積
+        return a*b*c
+           
+ 
+#呼叫副函式，先建立類別物件
+obj1 = A()
+#再利用建立好的類別物件obj1呼叫副函式method1
+a=1
+b=2
+c=3
+result = obj1.method1(a, b, c) #呼叫副函式method1，將結果存入變數result中
+print(result) #列印結果
+
+"""
