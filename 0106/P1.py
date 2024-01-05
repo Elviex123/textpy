21e8e2af21ea0295cc49bfd5e7ccc08d10196664
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-01-05 09:03:09 +0800

    2

--- 0106/P1.py
+++ 0106/P1.py
@@ -17,20 +17,19 @@ class Game:
     def get_game_people(self):
         return self.game_people
     
-    
 G1=Game('遊戲1','2:00','10')
 G2=Game('遊戲2','5:30','12')
 G3=Game("遊戲3",'8:00','8')
 
-print("遊戲名稱-1:",G1.get_game_name())
-print("開始時間-1:",G1.get_start_time())
-print("玩家人數-1:",G1.get_game_people())
+print("1-遊戲名稱:",G1.get_game_name())
+print("1-開始時間:",G1.get_start_time())
+print("1-玩家人數:",G1.get_game_people())
 
-print("遊戲名稱-2:",G2.get_game_name())
-print("開始時間-2:",G2.get_start_time())
-print("玩家人數-2:",G2.get_game_people())
+print("2-遊戲名稱:",G2.get_game_name())
+print("2-開始時間:",G2.get_start_time())
+print("2-玩家人數:",G2.get_game_people())
 
-print("遊戲名稱-3:",G3.get_game_name())
-print("開始時間-3:",G3.get_start_time())
-print("玩家人數-3:",G3.get_game_people())
+print("3-遊戲名稱:",G3.get_game_name())
+print("3-開始時間:",G3.get_start_time())
+print("3-玩家人數:",G3.get_game_people())
         
---
+++ 0106/P2.py
@@ -0,0 +1,5 @@
+class Luggage:
+    def __init__(self,lg_id,ig_weight,lg_start_airport,lg_last_airport,lg_name):
+        self.lg_id = lg_id
+
+#列印資訊
